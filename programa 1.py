adbf9cd9f642051cc7546c28f329fc22527fda0b
emilio1278 <90073059+emilio1278@users.noreply.github.com>
2021-09-24 21:12:52 -0300

    Add files via upload

---
+++ programa 1.py
@@ -0,0 +1,17 @@
+print("----------------------------------------------")
+print("creditos mandell")
+print("----------------------------------------------")
+
+varnombre = input("ingrese su nombre:")
+varapellido = input("ingrese su apellido:")
+varedad = input("ingrese su edad:")
+varmontosolicitud = input("ingrese su monto credito:")
+varsalario = input ("ingrese su sueldo mensual:")
+
+if varmontosolicitud > varsalario:
+    print("credito supera su sueldo")
+else:
+    print("credito aprobado")
+    print("si no paga!!")
+    print("le matamos al perrito")
+    print("le matamos el gato a cachas")
