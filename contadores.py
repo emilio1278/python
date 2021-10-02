cf68d86b1d7e5f75c1416cddafb45e5937cbc500
emilio1278 <90073059+emilio1278@users.noreply.github.com>
2021-10-01 21:11:24 -0300

    Add files via upload

---
+++ contadores.py
@@ -0,0 +1,31 @@
+varnumero1 = 0
+varincrementador = 1
+
+varnumero1 = varnumero1 + varincrementador
+
+print(varnumero1)
+
+print("el pipe puro weon")
+
+varnumero1 = varnumero1 + varincrementador
+
+print(varnumero1)
+
+print ("don mandell puro wilson")
+
+varnumero1 = varnumero1 + varincrementador
+
+print(varnumero1)
+
+print ("-------------------------")
+
+varRango1 = 0
+varRango2 = 3
+
+for varCont in range(varRango1,varRango2): 
+    print("la secuencia es:"+str(varCont))
+
+    varArreglo = [a,e,i,o,u]
+
+    for varCont1 in varArreglo:
+    print("las vocales son: "+varArreglo)
